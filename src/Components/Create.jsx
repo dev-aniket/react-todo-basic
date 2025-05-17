648636394c0e64efb1cc376cfe3edf2a990e8c9d
Aniket Mallick <aniketmallick0007@gmail.com>
2025-05-17 13:12:38 +0530

    Form Validation

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,32 +1,49 @@
 import React, { useState } from 'react'
 import { nanoid } from 'nanoid'
+import { useForm } from 'react-hook-form';
+
+
 const Create = (props) => {
 
     let todos = props.todos;
     let setTodos = props.setTodos;
-    const[title, setTitle] = useState("")
+    // const[title, setTitle] = useState("")
 
-    const handleSubmit = (e) =>{
-    e.preventDefault();
+  //   const handleSubmit = (e) =>{
+  //   e.preventDefault();
 
-    const newTodo = {
-      title: title,
-      id: nanoid(),
-      isComplete : false
-    }
+  //   const newTodo = {
+  //     title: title,
+  //     id: nanoid(),
+  //     isComplete : false
+  //   }
 
-    setTodos([...todos, newTodo]);
+  //   setTodos([...todos, newTodo]);
 
-    setTitle("")
+  //   setTitle("")
+  // }
+
+  const{register, handleSubmit, reset, formState:{errors}} = useForm();
+
+   const submitHandler = (data) => {
+    data.id = nanoid();
+    data.isComplete = false;
+    let copyData = [...todos]
+    copyData.push(data);
+
+    setTodos(copyData);
+
+    reset();
   }
 
   return (
     <div className='border w-[70%] p-10 flex flex-col text-center gap-4'>
         <h1 className='text-5xl font-thin' >Set <span className='text-red-400'> Remainder </span> for Task</h1>
-        <form onSubmit={handleSubmit}>
-          <input className='outline-0 p-2 border-b w-full text-2xl font-thin' type="text" placeholder='Enter Task' onChange={(e) => setTitle(e.target.value)} value={title}/>
+        <form onSubmit={handleSubmit(submitHandler)}>
+          <input {...register("title", {required:"Title cannot be empty"})} className='outline-0 p-2 border-b w-full text-2xl font-thin' type="text" placeholder='Enter Task'/>
           <br />
           <button className='px-10 py-2 border rounded text-xl mt-5'>Add Task</button>
+          <small>{errors?.title?.message}</small>
         </form>
   
     </div>
